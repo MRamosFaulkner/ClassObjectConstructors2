7a4303285b71308ca29f3c4ab5464ad52fd02c28
unknown <dnal03@msn.com>
2023-05-12 22:34:16 -0400

    banking updated

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,70 +1,56 @@
 
-import java.util.Scanner;
-
 public class BankAccount {
+    private double balance;
+    private String accountHolder;
+    private int accountNumber;
 
-    //Attributes that can only be used in this class (private)
-    private String acctHolderName;
-    private double acctBalance;
-    private int acctNumber;
+    public BankAccount() {}
 
-    //Constructor that takes the attributes as parameters to initialize
-    public BankAccount(String acctHolderName, double acctBalance) {
-        this.acctHolderName = acctHolderName;
-        this.acctBalance = acctBalance;
-        this.acctNumber = acctNumber;
+    public BankAccount(String accountHolderName, double balance, int accountNumber) {
+        accountHolder = accountHolderName;
+        this.balance = balance;
+        this.accountNumber = accountNumber;
     }
 
-    public BankAccount() {
-        Scanner scanner = new Scanner(System.in);
-
-        String nameOfHolder;
-        double startingBal;
-
-        System.out.println("Are you an existing customer? (-1 to exit) \n1. Yes \n2.No");
-        int answer = scanner.nextInt();
-
-        if (answer == 1) {
-            System.out.println("Please enter your account number: ");
-            int acctNumber = scanner.nextInt();
-
-            this.acctBalance = 1000.0;
-            this.acctHolderName = "John Doe";
-            this.acctNumber = acctNumber;
-        } else if (answer == 2) {
-            System.out.println("Let's make an account!");
-
-            System.out.print("What is the beginning balance for the account? ");
-            this.acctBalance = scanner.nextDouble();
-
-            System.out.println("Account created successfully.");
-        } else {
-            System.out.println("Exiting program.");
-            System.exit(0);
-        }
+    public void setName(String name) {
+        accountHolder = name;
     }
 
-
-    //Different methods created
-    public void deposit(double depositAmnt) {
-        acctBalance += depositAmnt;
+    public void setBalance(double balance) {
+        this.balance = balance;
     }
 
-    public void withdrawal (double amount) {
-//        if (amount > this.acctBalance) {
-//            System.out.println("Insufficient funds");
-//        } else {
-//            this.acctBalance = amount;
-//        }
-        acctBalance -= amount;
+    public void setAccountNumber(int num) {
+        accountNumber = num;
     }
 
-    public void printDetails() {
-        System.out.println("My balance is " + this.acctBalance + "\n");
+    public String getName() {
+        return accountHolder;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void deposit(double depositAmount) {
+        balance += depositAmount;
+    }
+
+    public void withdrawal(double withdrawalAmount) {
+        balance -= withdrawalAmount;
+    }
+
+    public void transfer(BankAccount a, double amount) {
+        this.withdrawal(amount);
+        a.deposit(amount);
     }
 
     public String toString() {
-        return acctHolderName + "'s account balance: " + acctBalance;
+        return ("\n" + accountHolder + "'s account balance: $" + balance);
     }
 
 }
